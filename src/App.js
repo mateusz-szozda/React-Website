c3ca246167e8e6f8e7fd35e9f1f9c3919c3217d6
mateusz-szozda <mateusz.szozda@pm.me>
2022-06-13 16:35:15 +0200

    Building the icon grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import {
+  createTheme,
+  ThemeProvider,
+  makeStyles,
+} from '@material-ui/core/styles';
 import NavBar from './components/NavBar';
+import Grid from './components/Grid';
 import './App.css';
+
+import SecurityIcon from '@material-ui/icons/Security';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import TrendingUpIcon from '@material-ui/icons/TrendingUp';
+import ImportExportIcon from '@material-ui/icons/ImportExport';
+import ComputerIcon from '@material-ui/icons/Computer';
+import HttpIcon from '@material-ui/icons/Http';
 
 const theme = createTheme({
   palette: {
@@ -30,6 +42,33 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <NavBar />
+        <Grid
+          icon={
+            <SecurityIcon
+              style={{ fill: '#4360A6', height: '125', width: '125' }}
+            />
+          }
+          title="Secure"
+          btnTitle="Show Me More"
+        />
+        <Grid
+          icon={
+            <EventNoteIcon
+              style={{ fill: '#448A76', height: '125', width: '125' }}
+            />
+          }
+          title="Performant"
+          btnTitle="Show Me More"
+        />
+        <Grid
+          icon={
+            <SecurityIcon
+              style={{ fill: '#D05B2D', height: '125', width: '125' }}
+            />
+          }
+          title="Reliable"
+          btnTitle="Show Me More"
+        />
       </ThemeProvider>
     </div>
   );
